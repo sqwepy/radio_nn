42c77cf488ef89da781c1390b7696ab8e3cf6a10
Pranav <pranav.sampathkumar@gmail.com>
2023-09-14 17:05:34 +0200

    Add antenna filter to ignore points on the x axis
    Since we are seperating ce and ge, on the x axis (in the vb, vvb system)
    the seperation is ill-defined and the pulses are all over the place.
    It is better if the network isnt trained in these pulses.

--- radioNN/data/filters.py
+++ radioNN/data/filters.py
@@ -22,10 +22,16 @@ class DefaultFilter:
         self.output = output
         self.percentage = percentage
 
-    def get_antenna_indices(self):
-        return np.arange(240)
+    def _get_antenna_indices(self):
+        x_pos = np.abs(self.antenna_pos[self.shower_indices, :, 0])
+        y_pos = np.abs(self.antenna_pos[self.shower_indices, :, 1])
+        self.antenna_mask = (np.abs(np.arctan2(y_pos, x_pos)) > 0.5).flatten()
+        # return np.tile(np.arange(240), self.shower_indices.shape[0])
+        return np.tile(np.arange(240), self.shower_indices.shape[0])[
+            self.antenna_mask
+        ]
 
-    def get_shower_indices(self):
+    def _get_shower_indices(self):
         num_samples = int(self.input_data.shape[0] * self.percentage / 100)
         return np.random.choice(
             np.arange(self.input_data.shape[0]),
@@ -33,12 +39,16 @@ class DefaultFilter:
             replace=False,
         )
 
-    def get_indices(
-        self,
-    ):
-        shower_indices = self.get_shower_indices()
-        antenna_indices = self.get_antenna_indices()
-        indices = np.repeat(shower_indices, len(antenna_indices)) * len(
-            antenna_indices
-        ) + np.tile(antenna_indices, shower_indices.shape[0])
+    def get_indices(self):
+        self.shower_indices = self._get_shower_indices()
+        self.antenna_indices = self._get_antenna_indices()
+        print(self.antenna_indices.shape)
+        print(self.shower_indices.shape)
+        indices = (
+            np.repeat(
+                self.shower_indices,
+                240,
+            )
+            * 240
+        )[self.antenna_mask] + self.antenna_indices
         return np.sort(indices)
