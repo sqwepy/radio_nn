27f4fcfdb54215f89093ce3fa561e2fe7f511479
sqwepy <zufettzumlebena@gmail.com>
2025-02-11 16:45:33 +0100

    Changed Values of Parameters and Replaced Values with variables

--- init_npy.py
+++ init_npy.py
@@ -5,34 +5,74 @@ Create npy files to be used by open_memmap
 import numpy as np
 import os
 
-RADIO_DATA_PATH = "/home/pranav/work-stuff-unsynced/radio_data"
-file_name = os.path.join(RADIO_DATA_PATH, "input_data")
-os.remove(file_name)
-fp1 = np.memmap(file_name, dtype="float32", mode="w+", shape=(2158 * 27, 300, 11))
-print(fp1.shape)
-np.save(file_name, fp1)
-file_name = os.path.join(RADIO_DATA_PATH, "meta_data")
-os.remove(file_name)
-fp2 = np.memmap(file_name, dtype="float32", mode="w+", shape=(2158 * 27, 13))
-print(fp2.shape)
-np.save(file_name, fp2)
-file_name = os.path.join(RADIO_DATA_PATH, "antenna_pos_data")
-os.remove(file_name)
-fp3 = np.memmap(file_name, dtype="float32", mode="w+", shape=(2158 * 27, 240, 3))
-print(fp3.shape)
-np.save(file_name, fp3)
-file_name = os.path.join(RADIO_DATA_PATH, "output_meta_data")
-os.remove(file_name)
-fp4 = np.memmap(file_name, dtype="float32", mode="w+", shape=(2158 * 27, 240, 2))
-print(fp4.shape)
-np.save(file_name, fp4)
-file_name = os.path.join(RADIO_DATA_PATH, "output_gece_data")
-os.remove(file_name)
-fp5 = np.memmap(file_name, dtype="float32", mode="w+", shape=(2158 * 27, 240, 256, 2))
-print(fp5.shape)
-np.save(file_name, fp5)
-file_name = os.path.join(RADIO_DATA_PATH, "output_vBvvB_data")
-os.remove(file_name)
-fp6 = np.memmap(file_name, dtype="float32", mode="w+", shape=(2158 * 27, 240, 256, 3))
-print(fp6.shape)
-np.save(file_name, fp6)
+RADIO_DATA_PATH = "/Users/denis/Desktop/BachelorThesis/memmaps/test"
+
+def _init_(path_,total_amount_of_measurements = 33, parameters = 11, event_level_parameters = 13, number_of_antennas = 160,grammage_steps = 1142, time_bins = 256, dimensions_antenna_positions_vB_vvB = 3, dimensions_antenna_traces_vB_vvB = 3,dimensions_antenna_traces_ge_ce = 2,time_ge_ce_and_vB_vvB = 2):
+    '''
+    Parameters:
+    
+        path_: path where the memmap files should be created
+
+        total_amount_of_measurements: proton + iron measurements (total amount of SIM files)
+
+        parameters: used parameters
+
+        event_level_parameters: used event level parameters
+
+        number_of_antennas: the total number of antennas
+
+        grammage_steps: how many X steps were taken
+
+        time_bins: how many time_bins there is
+
+        dimensions_antenna_positions_vB_vvB: XYZ
+
+        dimensions_antenna_traces_vB_vvB: XYZ,
+
+        dimensions_antenna_traces_ge_ce: X' Y'
+
+        time_ge_ce_and_vB_vvB: time of ge_ce and vB_vvB
+    
+    '''
+    dtypeInit = "float32"
+    
+    file_name = os.path.join(path_, "input_data")
+    #os.remove(file_name)
+    #(number of simulations, grammage steps, parameters)
+    # for iron 12, proton 21
+    fp1 = np.memmap(file_name, dtype=f"{dtypeInit}", mode="w+", shape=(total_amount_of_measurements, grammage_steps, parameters))
+    print(fp1.shape)
+    np.save(file_name, fp1)
+    file_name = os.path.join(path_, "meta_data")
+    #os.remove(file_name)
+    #(number of simulations, event level parameters)
+    fp2 = np.memmap(file_name, dtype=f"{dtypeInit}", mode="w+", shape=(total_amount_of_measurements, event_level_parameters))
+    print(fp2.shape)
+    np.save(file_name, fp2)
+    file_name = os.path.join(path_, "antenna_pos_data")
+    #os.remove(file_name)
+    #(number of simulations, number of antennas, ant positions)
+    fp3 = np.memmap(file_name, dtype=f"{dtypeInit}", mode="w+", shape=(total_amount_of_measurements, number_of_antennas, dimensions_antenna_positions_vB_vvB))
+    print(fp3.shape)
+    np.save(file_name, fp3)
+    file_name = os.path.join(path_, "output_meta_data")
+    #os.remove(file_name)
+    #(number of simulations, number of antennas, time (s))
+
+    fp4 = np.memmap(file_name, dtype=f"{dtypeInit}", mode="w+", shape=(total_amount_of_measurements, number_of_antennas, time_ge_ce_and_vB_vvB))
+    print(fp4.shape)
+    np.save(file_name, fp4)
+    file_name = os.path.join(path_, "output_gece_data")
+    #os.remove(file_name)
+    fp5 = np.memmap(file_name, dtype=f"{dtypeInit}", mode="w+", shape=(total_amount_of_measurements, number_of_antennas, time_bins, dimensions_antenna_traces_ge_ce))
+    print(fp5.shape)
+    np.save(file_name, fp5)
+    #(number of simulations, number of antennas, time bins, polarizations)
+    file_name = os.path.join(path_, "output_vBvvB_data")
+    #os.remove(file_name)
+    fp6 = np.memmap(file_name, dtype=f"{dtypeInit}", mode="w+", shape=(total_amount_of_measurements, number_of_antennas, time_bins, dimensions_antenna_traces_vB_vvB))
+    print(fp6.shape)
+    np.save(file_name, fp6)
+    
+if __name__ == "__main__":    #PLAYING CODE
+    _init_(RADIO_DATA_PATH)
