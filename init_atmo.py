92e6c6ffb1a333f1d4b2fc1e9a431223b0a0f499
sqwepy <zufettzumlebena@gmail.com>
2025-04-15 15:15:24 +0200

    curved=false

--- init_atmo.py
+++ init_atmo.py
@@ -3,8 +3,8 @@ import numpy as np
 import traceback
 from radiotools.atmosphere.models import Atmosphere
 
-lofar_sim_path = '/Users/denis/Desktop/BachelorThesis/data'
-atm_dir_path = '/Volumes/DenisDRIVE/BACHELORTHESIS/atmospheric_data/atmosphere_files'
+lofar_sim_path = '/home/denis/lofar_hdf5_sims_batch2'
+atm_dir_path = '/home/denis/atmosphere_files'
 
 for isim, sim_dir in enumerate(os.listdir(lofar_sim_path)):
     if sim_dir.isdigit() == False:
@@ -14,7 +14,7 @@ for isim, sim_dir in enumerate(os.listdir(lofar_sim_path)):
     atm_file_path = os.path.join(atm_dir_path, f"ATMOSPHERE_{sim_dir}.DAT")
     try:
         try:
-            atm_model = Atmosphere(gdas_file=atm_file_path, curved=True)
+            atm_model = Atmosphere(gdas_file=atm_file_path, curved=False)
         except FileNotFoundError:
             print(f"Atmosphere file not found for {sim_dir}")
             continue
